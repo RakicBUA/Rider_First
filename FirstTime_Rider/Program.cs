7304e16af67d4f0fdfe24bc368b3e2763ff59bb7
Rakic_Jugo <rakic.nemanja@hak-liezen.at>
2022-03-28 20:53:18 +0200

    different types of lists
    with rider

---
+++ FirstTime_Rider/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstTime_Rider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using different Types of lists
+            
+            //int Array
+
+            int[] intArray = new int[4] {3, 4, 5, 2};
+            
+            // print out the numbers
+
+            foreach (var item in intArray)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
